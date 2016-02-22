3995c8ace723390ee51e42b60e7e32ffd5ffc8b4
JoeWroe <joe_wroe@icloud.com>
2016-02-22 17:43:48 +0000

    Added Temp

--- src/thermostatJQuery.js
+++ src/thermostatJQuery.js
@@ -2,7 +2,11 @@ $( document ).ready(function() {
 
   var thermostat = new Thermostat();
 
-  $("#power-saving-status").text(thermostat.POWER_SAVING);
+  $("#powersaving").click(function() {
+    thermostat.powerSavingButton();
+    $("#power-saving-status").text(thermostat.POWER_SAVING);
+  });
 
-  //  thermostat.POWER_SAVING);
+
+
 });
