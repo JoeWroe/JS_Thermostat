c437ace355c1938deb5ce97f597381705da18884
JoeWroe <joe_wroe@icloud.com>
2016-02-22 17:49:29 +0000

    JQuery for changing and resetting temp

--- src/thermostatJQuery.js
+++ src/thermostatJQuery.js
@@ -7,6 +7,21 @@ $( document ).ready(function() {
     $("#power-saving-status").text(thermostat.POWER_SAVING);
   });
 
+  $("#temperature-up").click(function() {
+    thermostat.upButton();
+    $("#currentTemperature").text(thermostat.temperature);
+  });
+
+  $("#temperature-down").click(function() {
+    thermostat.downButton();
+    $("#currentTemperature").text(thermostat.temperature);
+  });
+
+  $("#temperature-reset").click(function() {
+    thermostat.tempReset();
+    $("#currentTemperature").text(thermostat.temperature);
+  });
+
 
 
 });
