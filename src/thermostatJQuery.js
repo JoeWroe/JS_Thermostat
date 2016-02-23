843737f17e9d11bdb76d995299beb73887db85ae
JoeWroe <joe_wroe@icloud.com>
2016-02-23 12:38:46 +0000

    TEMPERATURE UPDATES
    Temp changes colour
    Current Temperature included

--- src/thermostatJQuery.js
+++ src/thermostatJQuery.js
@@ -2,32 +2,46 @@ $( document ).ready(function() {
 
   var thermostat = new Thermostat();
 
-  // $fadeText = function() {
-  // $("#currentTemperature").animate({
-  //   opacity : 0
-  // }, 150, function() {
-  //   $("#currentTemperature").text(thermostat.temperature);
-  // });
+  function updateTemperature() {
+    $("#currentTemperature").text(thermostat.temperature);
+    if(thermostat.COLOUR === "Green") {
+      $("#currentTemperature").css('color', '#90EE90');
+    } else if (thermostat.COLOUR === "Yellow") {
+      $("#currentTemperature").css('color', '#F0E68C');
+    } else {
+      $("#currentTemperature").css('color', '#F08080');
+    }
+  }
+
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=London&appid=81e11cff7d2f945e47fccbf7619304cf&units=metric', function(data) {
+    $('#current-temperature').text(data.main.temp);
+  });
 
   $("#powersaving").click(function() {
-    thermostat.powerSavingButton();
-    $("#power-saving-status").text(thermostat.POWER_SAVING);
+    $("#power-saving-status").fadeOut(1000, function() {
+      thermostat.powerSavingButton();
+      $(this).text(thermostat.POWER_SAVING);
+    }).fadeIn(2000);
   });
 
   $("#temperature-up").click(function() {
-    thermostat.upButton();
-    $("#currentTemperature").text(thermostat.temperature);
+    $("#currentTemperature").fadeOut(500, function() {
+      thermostat.upButton();
+      updateTemperature();
+    }).fadeIn(1000);
   });
 
   $("#temperature-down").click(function() {
-    thermostat.downButton();
-    $("#currentTemperature").text(thermostat.temperature);
+    $("#currentTemperature").fadeOut(500, function() {
+      thermostat.downButton();
+      updateTemperature();
+    }).fadeIn(1000);
   });
 
   $("#temperature-reset").click(function() {
     $("#currentTemperature").fadeOut(1000, function() {
       thermostat.tempReset();
-      $(this).text(thermostat.temperature);
+      updateTemperature();
     }).fadeIn(2000);
   });
 });
